bea55b27c4b01988a68d595a24bb79f6cec64ceb
ses167 <ses167@pitt.edu>
2016-11-02 19:03:36 -0400

    Main Method Updated

--- Data_Generator.java
+++ Data_Generator.java
@@ -8,14 +8,49 @@ public class Data_Generator
 	public static void main(Strings[] args)
 	{
 		Random numGen = new Random();
-		int pick = numGen.nextInt(100) + 1;
-		int counter = 0;
+		int pick;						// Random Number
+		int counter = 0;				// User Counter
 		
-		
-		// while(counter < 200)
-// 		{
-// 			System.out.printf("INSERT INTO CUSTOMER VALUES (%08d
-// 		}
+		while(counter < 200)
+		{
+			pick1 = numGen.nextInt(100) + 1;
+			pick2 = numGen.nextInt(20) + 1;
+			int tmp = counter + 17;
+			
+			// CID
+			System.out.printf("INSERT INTO CUSTOMER VALUES (%08d, ", tmp);
+			
+			// SALUTATION, F_NAME, L_NAME
+			if(pick1 > 50)
+			{
+				maleName(pick1 - 50, pick2);
+			}
+			else
+			{
+				femaleName(pick1, pick2);
+			}
+			
+			// FREQUENT MILES
+			frequentMiles(pick1 % 20);
+			
+			// CREDIT CARD NUMBER
+			ccNum();
+			
+			// CREDIT CARD DATE
+			ccDate((pick2 % 12), (pick1 - 1));			// Month, Year
+			
+			// STREET NAME
+			streetName(pick2 % 10 + 11);
+			
+			// CITY AND STATE
+			cityState(pick1 % 20);
+			
+			// PHONE
+			phone();
+			
+			// EMAIL
+			email(tmp);
+		}
 	}
 	
 	// Generate Mrs. or Ms. modularly with a first and last name.
