3744a4df2da437686d0cf643a694f1c617a6d8a8
ses167 <ses167@pitt.edu>
2016-11-02 18:25:19 -0400

    Data_Generator Started

---
+++ Data_Generator.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+public class Data_Generator
+{
+	public static void main(Strings[] args)
+	{
+		int counter = 0;
+		
+		
+		// while(counter < 200)
+// 		{
+// 			System.out.printf("INSERT INTO CUSTOMER VALUES (%08d
+// 		}
+	}
+	
+	// Generate Mrs. or Ms. modularly with a first and last name.
+	// Print result
+	public static void femaleName()
+	{
+	}
+	
+	// Generate random first and last name
+	// Print result
+	public static void maleName()
+	{
+	}
+	
+	// Generates y/n modularly
+	public static void frequentMiles()
+	{
+	}
+	
+	// Generate random 16 digit credit card number
+	public static void ccNum()
+	{
+	}
+	
+	// Generate "to_date("<rand_mon>/<rand_year>", "MM/YY")"
+	public static void ccDate()
+	{
+	}
+	
+	// Randomly select name from list
+	public static void streetName()
+	{
+	}
+	
+	// Randomly select city and state from list
+	public static void cityState()
+	{
+	}
+	
+	// Randomly generate phone number
+	public static void phone()
+	{
+	}
+	
+	// Use first and last name to create email with cid "@pitt.edu"
+	public static void email()
+	{
+	}
+}
