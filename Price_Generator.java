e664c3cd6348ace981b945960c8f62ddd83ded93
ses167 <ses167@pitt.edu>
2016-11-07 21:20:43 -0500

    Data Changes

--- Price_Generator.java
+++ Price_Generator.java
@@ -5,12 +5,18 @@ import java.io.*;
 
 public class Price_Generator
 {
+	private static final int DATA_COUNT = 100;
 	private static PrintWriter output;
 	private static String[] cities = {"PIT", "IND", "NYC", "MCO", "LAS", "JFK", "MIA", "HOU", "ATL", "DAL", "HON", "SEA", "DET", "CHI", "PHX", "CLT", "COL", "BOS", "DEN", "ANC"};	
 	private static String[] airlineID = {"10001", "20001", "30001", "40001", "50001", "60001", "70001", "80001", "90001", "10010"};
 
+	private static String[] arrCity;
+	private static String[] depCity;
 	public Price_Generator()
 	{
+		arrCity = new String[DATA_COUNT];
+		depCity = new String[DATA_COUNT];
+		
 		try
 		{
 			output = new PrintWriter("price_data.sql");
@@ -23,13 +29,13 @@ public class Price_Generator
 		Random numGen = new Random();
 		int counter = 0;				// Counter for while loop
 		
-		while(counter < 100)
+		while(counter < DATA_COUNT)
 		{	
 			// BEGIN
 			output.print("INSERT INTO PRICE VALUES (");
 
 			// DEPARTURE AND ARRIVAL CITY
-			departure_arrival_city(numGen);
+			departure_arrival_city(numGen, counter);
 			
 			// AIRLINE ID
 			airlineID(numGen);
@@ -45,7 +51,7 @@ public class Price_Generator
 	}
 	
 	// Generate Departure and Arrival Cities
-	private static void departure_arrival_city(Random gen)
+	private static void departure_arrival_city(Random gen, int index)
 	{
 		int num1 = gen.nextInt(999) + 1;
 		int num2;
@@ -53,12 +59,28 @@ public class Price_Generator
 		String arrival_code;
 		do {
 			num2 = gen.nextInt(999) + 1;
-		}while(cities[num1 % 20].equals(cities[num2 % 20]));
-		
+		}while((cities[num1 % 20].equals(cities[num2 % 20])) || (checkDup(num1, num2, index)));
+
 		arrival_code = cities[num2 % 20];
+		
+		arrCity[index] = arrival_code;
+		depCity[index] = depart_code;
 		
 		output.printf("'%s', ", depart_code);
 		output.printf("'%s', ", arrival_code);
+	}
+	
+	// Check Duplicate Flights
+	private static boolean checkDup(int num1, int num2, int index)
+	{
+		for(int i = 0; i < index; i++)
+		{
+			if(arrCity[i].equals(cities[num2%20]) && depCity[i].equals(cities[num1%20]))
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	// Generate Airline Number
