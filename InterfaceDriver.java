abb5c8553e64107888063af97e96b74b2398fb27
ses167 <ses167@pitt.edu>
2016-12-09 23:19:24 -0500

    Creating Data Files for Interface Driver

--- InterfaceDriver.java
+++ InterfaceDriver.java
@@ -40,118 +40,20 @@ public class InterfaceDriver
 		}
 		
 		/******** ADMINISTRATOR ********/
-		
-		// Task 2 -- Load Airline Information
-		String task2File = "admin_task2_test.txt";
-		System.out.println("Testing Admin Task 2... Filename: " + task2File);
-		
+		// Task 1 -- Delete Database (To Begin Fresh)
 		try
 		{
-			String before = "SELECT COUNT(*) FROM Airline";
-			rs = statement.executeQuery(before);
-			System.out.printf("Cardinality of Airline Before Insert: %s", rs.next().getString("COUNT(*)"));
+			admin_task1("yes");
+			System.out.println("Database cleared");
 		}
 		catch(SQLException err)
 		{
 			System.out.println(err.toString());
-		}
-		
-		int t2 = pti.admin_task2(task2File);
-		
-		if(t2 != 0)
-		{
-			System.out.println("The data was not inserted into the database.");
-			System.out.println("Admin Task 2 Test Failed.");
-		}
-		else
-		{
-			try
-			{
-				String after = "SELECT COUNT(*) FROM Airline";
-				rs = statement.executeQuery(after);
-				System.out.printf("Cardinality of Airline After Insert: %s", rs.next().getString("COUNT(*)"));
-			}
-			catch(SQLException err)
-			{
-				System.out.println(err.toString());
-			}
-		}
-		
-		// Task 3 -- Load Schedule Information
-		String task3File = "admin_task3_test.txt";
-		System.out.println("Testing Admin Task 3... Filename: " + task3File);
-		
-		try
-		{
-			String before = "SELECT COUNT(*) FROM Flight";
-			rs = statement.executeQuery(before);
-			System.out.printf("Cardinality of Flight Before Insert: %s", rs.next().getString("COUNT(*)"));
-		}
-		catch(SQLException err)
-		{
-			System.out.println(err.toString());
-		}
-		
-		int t3 = pti.admin_task3(task3File);
-		
-		if(t3 != 0)
-		{
-			System.out.println("The data was not inserted into the database.");
-			System.out.println("Admin Task 3 Test Failed.");
-		}
-		else
-		{
-			try
-			{
-				String after = "SELECT COUNT(*) FROM Flight";
-				rs = statement.executeQuery(after);
-				System.out.printf("Cardinality of Flight After Insert: %s", rs.next().getString("COUNT(*)"));
-			}
-			catch(SQLException err)
-			{
-				System.out.println(err.toString());
-			}
-		}
-		
-		// Task 4 -- Load Price Information
-		String task4File = "admin_task4_test.txt";
-		System.out.println("Testing Admin Task 4... Filename: " + task4File);
-		
-		try
-		{
-			String before = "SELECT COUNT(*) FROM Price";
-			rs = statement.executeQuery(before);
-			System.out.printf("Cardinality of Price Before Insert: %s", rs.next().getString("COUNT(*)"));
-		}
-		catch(SQLException err)
-		{
-			System.out.println(err.toString());
-		}
-		
-		int t4 = pti.admin_task4(task4File);
-		
-		if(t4 != 0)
-		{
-			System.out.println("The data was not inserted into the database.");
-			System.out.println("Admin Task 4 Test Failed.");
-		}
-		else
-		{
-			try
-			{
-				String after = "SELECT COUNT(*) FROM Price";
-				rs = statement.executeQuery(after);
-				System.out.printf("Cardinality of Price After Insert: %s", rs.next().getString("COUNT(*)"));
-			}
-			catch(SQLException err)
-			{
-				System.out.println(err.toString());
-			}
 		}
 		
 		// Task 5 -- Load Plane Information
-		String task5File = "admin_task5_test.txt";
-		System.out.println("Testing Admin Task 5... Filename: " + task5File);
+		String[] task5File = {"admin_5.1.txt", "admin_5.2.txt", "admin_5.3.txt", "admin_5.4.txt"};
+		System.out.println("Testing Admin Task 5...");
 		
 		try
 		{
@@ -164,24 +66,145 @@ public class InterfaceDriver
 			System.out.println(err.toString());
 		}
 		
-		int t5 = pti.admin_task5(task5File);
+		for(String taskFile : task5File)
+		{
+			int t5 = pti.admin_task5(taskFile);
 		
-		if(t5 != 0)
-		{
-			System.out.println("The data was not inserted into the database.");
-			System.out.println("Admin Task 5 Test Failed.");
-		}
-		else
-		{
-			try
+			if(t5 != 0)
 			{
-				String after = "SELECT COUNT(*) FROM Plane";
-				rs = statement.executeQuery(after);
-				System.out.printf("Cardinality of Plane After Insert: %s", rs.next().getString("COUNT(*)"));
+				System.out.println("The data was not inserted into the database.");
+				System.out.println("Admin Task 5 Test Failed.");
 			}
-			catch(SQLException err)
+			else
 			{
-				System.out.println(err.toString());
+				try
+				{
+					String after = "SELECT COUNT(*) FROM Plane";
+					rs = statement.executeQuery(after);
+					System.out.printf("Cardinality of Plane After Insert: %s", rs.next().getString("COUNT(*)"));
+				}
+				catch(SQLException err)
+				{
+					System.out.println(err.toString());
+				}
+			}
+		}
+		
+		// Task 2 -- Load Airline Information
+		String[] task2File = {"admin_2.1.txt", "admin_2.2.txt", "admin_2.3.txt"};
+		System.out.println("Testing Admin Task 2...");
+		
+		try
+		{
+			String before = "SELECT COUNT(*) FROM Airline";
+			rs = statement.executeQuery(before);
+			System.out.printf("Cardinality of Airline Before Insert: %s", rs.next().getString("COUNT(*)"));
+		}
+		catch(SQLException err)
+		{
+			System.out.println(err.toString());
+		}
+		
+		for(String taskFile : task2File)
+		{
+			int t2 = pti.admin_task2(taskFile);
+		
+			if(t2 != 0)
+			{
+				System.out.println("The data was not inserted into the database.");
+				System.out.println("Admin Task 2 Test Failed.");
+			}
+			else
+			{
+				try
+				{
+					String after = "SELECT COUNT(*) FROM Airline";
+					rs = statement.executeQuery(after);
+					System.out.printf("Cardinality of Airline After Insert: %s", rs.next().getString("COUNT(*)"));
+				}
+				catch(SQLException err)
+				{
+					System.out.println(err.toString());
+				}
+			}
+		}
+		
+		// Task 3 -- Load Schedule Information
+		String[] task3File = {"admin_3.1.txt", "admin_3.2.txt", "admin_3.3.txt", "admin_3.4.txt", "admin_3.5.txt"};
+		System.out.println("Testing Admin Task 3...");
+		
+		try
+		{
+			String before = "SELECT COUNT(*) FROM Flight";
+			rs = statement.executeQuery(before);
+			System.out.printf("Cardinality of Flight Before Insert: %s", rs.next().getString("COUNT(*)"));
+		}
+		catch(SQLException err)
+		{
+			System.out.println(err.toString());
+		}
+		
+		for(String taskFile : task3File)
+		{
+			int t3 = pti.admin_task3(taskFile);
+		
+			if(t3 != 0)
+			{
+				System.out.println("The data was not inserted into the database.");
+				System.out.println("Admin Task 3 Test Failed.");
+			}
+			else
+			{
+				try
+				{
+					String after = "SELECT COUNT(*) FROM Flight";
+					rs = statement.executeQuery(after);
+					System.out.printf("Cardinality of Flight After Insert: %s", rs.next().getString("COUNT(*)"));
+				}
+				catch(SQLException err)
+				{
+					System.out.println(err.toString());
+				}
+			}
+		}
+		
+		// Task 4 -- Load Price Information
+		String[] task4File = {"admin_4.1.txt", "admin_4.2.txt", "admin_4.3.txt", "admin_4.4.txt", "admin_4.5.txt", 
+								"admin_4.6.txt", "admin_4.7.txt", "admin_4.8.txt", "admin_4.9.txt", "admin_4.10.txt"};
+		System.out.println("Testing Admin Task 4...");
+		
+		try
+		{
+			String before = "SELECT COUNT(*) FROM Price";
+			rs = statement.executeQuery(before);
+			System.out.printf("Cardinality of Price Before Insert: %s", rs.next().getString("COUNT(*)"));
+		}
+		catch(SQLException err)
+		{
+			System.out.println(err.toString());
+		}
+		
+		for(String taskFile : task4File)
+		{
+			int t4 = pti.admin_task4(taskFile);
+		
+			if(t4 != 0)
+			{
+				System.out.println("The data was not inserted into the database.");
+				System.out.println("Admin Task 4 Test Failed.");
+			}
+			else
+			{
+				try
+				{
+					String after = "SELECT COUNT(*) FROM Price";
+					rs = statement.executeQuery(after);
+					System.out.printf("Cardinality of Price After Insert: %s", rs.next().getString("COUNT(*)"));
+				}
+				catch(SQLException err)
+				{
+					System.out.println(err.toString());
+				}
 			}
 		}
 		
